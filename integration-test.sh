7d2e00d6d8be482ea63f81558d30b0a239273612
Your Name <you@example.com>
2022-08-31 13:48:20 +0530

    added lb ip

--- integration-test.sh
+++ integration-test.sh
@@ -12,7 +12,7 @@ then
 
     responce=$(curl -s $applicationURL:$PORT$applicationURI)
     http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
-    echo "Http status code: $http_code "
+
     if [[ "$responce" == 100 ]];
         then
             echo  "Increment test Passed"
@@ -21,13 +21,13 @@ then
             exit 1;
     fi
 
-    #if [[ "http_code" == 200 ]];
-    #    then
-    #        echo "HTTP status code test Passed"
-    #    else
-    #        echo "HTTP status code is not 200"
-    #        exit 1;
-    #fi
+    if [[ "$http_code" == 200 ]];
+        then
+            echo "HTTP status code test Passed"
+        else
+            echo "HTTP status code is not 200"
+            exit 1;
+    fi
 else
         echo "The service has issues"
         exit 1;
